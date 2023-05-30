e65b7aa6ca89b3b1c10936dd055378df51bba05b
Ali Forghani <a.forghani.y79@gmail.com>
2023-05-30 16:40:13 +0330

    init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM quay.io/keycloak/keycloak:21.1.1
+ENV KEYCLOAK_ADMIN=admin
+ENV KEYCLOAK_ADMIN_PASSWORD=admin
+#ADD --chown=keycloak:keycloak src/main/java/themes/* /opt/keycloak/themes
+ADD --chown=keycloak:keycloak ./target/*.jar /opt/keycloak/providers/
+EXPOSE 8080
+ENTRYPOINT ["/opt/keycloak/bin/kc.sh","start-dev"]
---
+++ src/main/java/ir/atlas/bimany/keycloak/authenticator/OTPAuthenticatorContstants.java
@@ -0,0 +1,10 @@
+package ir.atlas.bimany.keycloak.authenticator;
+
+public class OTPAuthenticatorContstants {
+    public static final String ANSW_SMS_CODE = "smsCode";
+
+    public static final String CONF_PRP_USR_ATTR_MOBILE = "sms-auth.attr.mobile";
+    public static final String CONF_PRP_SMS_CODE_LENGTH = "sms-auth.attr.length";
+    public static final String CONF_PRP_SMS_CODE_TTL = "sms-auth.attr.ttl";
+
+}
